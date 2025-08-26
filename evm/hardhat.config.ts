62552b5ab239ef321a288240bbeff2908019035f
Whitebluffblock <paulachilles67@gmail.com>
2025-08-26 12:20:57 +0800

    Create hardhat.config.ts

---
+++ evm/hardhat.config.ts
@@ -0,0 +1,10 @@
+{
+  "devDependencies": {
+    "hardhat": "^2.18.0",
+    "@nomicfoundation/hardhat-toolbox": "^3.0.0",
+    "ethers": "^6.0.0",
+    "ts-node": "^10.0.0",
+    "typescript": "^4.0.0",
+    "@openzeppelin/contracts": "^4.9.0"
+  }
+}
